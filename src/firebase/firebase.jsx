1f9d1cfbf2bf01bffed1cb80da9be9f8d73df5a0
sparshsinghal885 <sparshsinghal885@gmail.com>
2024-07-29 17:30:38 +0530

    add firebase

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEpfWxkQdikO1amTTOGpqhAAyGphujpyI",
+  authDomain: "shoe-website-594c0.firebaseapp.com",
+  projectId: "shoe-website-594c0",
+  storageBucket: "shoe-website-594c0.appspot.com",
+  messagingSenderId: "2496154307",
+  appId: "1:2496154307:web:37a25ac467f464e31c7076"
+};
+
+
+const app = initializeApp(firebaseConfig);
