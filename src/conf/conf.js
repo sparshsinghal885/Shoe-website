836b55dac21aa763e315db00bb9d2949a8b490cd
sparshsinghal885 <sparshsinghal885@gmail.com>
2024-08-09 03:27:29 +0530

    prepare for deployment

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+  apiKey: String(import.meta.env.VITE_API_KEY),
+  authDomain: String(import.meta.env.VITE_AUTH_DOMAIN),
+  projectId: String(import.meta.env.VITE_PROJECT_ID),
+  storageBucket: String(import.meta.env.VITE_STORAGE_BUCKET),
+  messagingSenderId: String(import.meta.env.VITE_MESSAGING_SENDER_ID),
+  appId: String(import.meta.env.VITE_API_ID)
+}
+
+export default conf
--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,17 +1,17 @@
-
+import conf from "@/conf/conf";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import {getStorage} from 'firebase/storage'
+import { getStorage } from 'firebase/storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEpfWxkQdikO1amTTOGpqhAAyGphujpyI",
-  authDomain: "shoe-website-594c0.firebaseapp.com",
-  projectId: "shoe-website-594c0",
-  storageBucket: "shoe-website-594c0.appspot.com",
-  messagingSenderId: "2496154307",
-  appId: "1:2496154307:web:37a25ac467f464e31c7076"
+  apiKey: conf.apiKey,
+  authDomain: conf.authDomain,
+  projectId: conf.projectId,
+  storageBucket: conf.storageBucket,
+  messagingSenderId: conf.messagingSenderId,
+  appId: conf.appId,
 };
 
 
@@ -21,4 +21,4 @@ const fireDB = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { fireDB, auth, storage}
+export { fireDB, auth, storage }
